1a4a842c0bc0a4c70d5fbdbb3a8144703cd8691b
Balint Handler <handlerbalint@gmail.com>
2021-03-18 20:46:21 +0100

    reading and storing inputs on button click

---
+++ myscript.js
@@ -0,0 +1,28 @@
+
+
+
+// Inputok tárolása
+function readInputs() {
+  const x = document.querySelector('#xInput');
+  const y = document.querySelector('#yInput');
+  console.log(x.value);
+  console.log(y.value);
+}
+readInputs();
+
+// Grid generálás
+
+
+// Gomb eseménykezelés
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', event => {
+  readInputs();
+  
+});
+// Mouse hover esemény
+
+
+// Random szín generálás
+
+
+
