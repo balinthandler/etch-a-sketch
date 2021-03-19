538a1f76c8f11113547e31d8aa2d913e32e6d6bd
Balint Handler <handlerbalint@gmail.com>
2021-03-19 20:49:51 +0100

    working grid generating based on input

--- myscript.js
+++ myscript.js
@@ -1,28 +1,49 @@
-
-
-
-// Inputok tárolása
+// Getting x and y from inputs
 function readInputs() {
   const x = document.querySelector('#xInput');
   const y = document.querySelector('#yInput');
-  console.log(x.value);
-  console.log(y.value);
+  const regEx = /^[1-9]\d?$/;
+  if (regEx.test(x.value) && regEx.test(y.value)) {
+    return [x.value, y.value];
+  } else {
+    alert('Type a number between 1 and 99!');
+  }
 }
-readInputs();
 
-// Grid generálás
+// Grid generating
+function createGrid(array) {
+  const grid = document.querySelector('#gridContainer');
+  grid.innerHTML = '';
+  const containerWidth = grid.clientWidth;
+  const containerHeight = grid.clientHeight;
+  for (j = 0; j < array[1]; j++){
+    const row = document.createElement('div');
+    row.classList.add('row');
+    grid.appendChild(row);
+    for (let i = 0; i < array[0]; i++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.style.width = ((containerWidth / array[0]) - 2)+ 'px';
+      cell.style.height = ((containerHeight / array[1]) - 2) + 'px';
+      row.appendChild(cell);
+    }
+
+  } 
 
 
-// Gomb eseménykezelés
+
+}
+
+// Gomb event handling
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', event => {
-  readInputs();
-  
+   
+   createGrid(readInputs());
+
 });
-// Mouse hover esemény
+// Mouseover handling
 
 
-// Random szín generálás
-
+// Random color generating for cells
 
 
