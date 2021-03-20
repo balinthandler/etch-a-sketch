4859089d9007f3fb2811b68f1309745173aacc16
Balint Handler <handlerbalint@gmail.com>
2021-03-20 11:15:55 +0100

    completed project

--- myscript.js
+++ myscript.js
@@ -1,8 +1,8 @@
-// Getting x and y from inputs
+// Returns array with 2 elements in it, Count X and Count Y
 function readInputs() {
-  const x = document.querySelector('#xInput');
-  const y = document.querySelector('#yInput');
-  const regEx = /^[1-9]\d?$/;
+  const x = document.querySelector('#inputX');
+  const y = document.querySelector('#inputY');
+  const regEx = /^[1-9]\d?$/; // between 1 and 99
   if (regEx.test(x.value) && regEx.test(y.value)) {
     return [x.value, y.value];
   } else {
@@ -10,7 +10,7 @@ function readInputs() {
   }
 }
 
-// Grid generating
+// Grid generating based on array[0] (Count X) and array[1] (Count Y)
 function createGrid(array) {
   const grid = document.querySelector('#gridContainer');
   grid.innerHTML = '';
@@ -26,42 +26,61 @@ function createGrid(array) {
       cell.style.width = ((containerWidth / array[0]) - 2)+ 'px';
       cell.style.height = ((containerHeight / array[1]) - 2) + 'px';
       row.appendChild(cell);
-      
     }
-    
   } 
   mouseOver();
-  
 }
 
-// ButtonClick event handling
-const btn = document.querySelector('#btn');
+
+// Create the grid on button click, based on input values
+const btn = document.querySelector('#button');
 btn.addEventListener('click', event => {
   createGrid(readInputs());
   
 });
-// Mouseover handling
+// Handling mouseover
 function mouseOver() {
   const cells = document.querySelectorAll('.cell');
   cells.forEach((cell) => {
     cell.addEventListener('mouseover', (event) => {
-      cell.style.backgroundColor = randomRGB();
-      console.log('hey');
+      // If cell color has been changed already
+      if (cell.style.backgroundColor !== ''){
+
+        // Get current rgb values, then decrease each of them by 25.5 (10% of 255)
+        let r = Math.floor(cell.style.backgroundColor.slice(4,cell.style.backgroundColor.indexOf(',')) - 25.5);
+        let g = Math.floor(cell.style.backgroundColor.slice(
+          cell.style.backgroundColor.indexOf(',', cell.style.backgroundColor.indexOf(',')) + 1,
+          cell.style.backgroundColor.lastIndexOf(',')) - 25.5);
+        let b = Math.floor(cell.style.backgroundColor.slice(cell.style.backgroundColor.lastIndexOf(',') + 1, 
+          cell.style.backgroundColor.lastIndexOf(')')) - 25.5);
+
+        if (r < 0)  { r = 0;};
+        if (g < 0)  { g = 0;};
+        if (b < 0)  { b = 0;};
+        
+        // Set new bgColor
+        cell.style.backgroundColor = `rgb(${r},${g},${b})`;
+
+        console.log(cell.style.backgroundColor);
+      
+      } else {
+        cell.style.backgroundColor = randomRGB();
+      }
     });
   });
 }
 
 
-// Random color generating for cells
+// Returns a random 'rgb(r,g,b)'
 function randomRGB() {
   let rgb = '';
   for (let i = 0; i<3; i++){
     if (i == 0) {
-      rgb += 'rgb(' + (Math.round(Math.random() * 255)) + ', ';
+      rgb += 'rgb(' + (Math.floor(Math.random() * 255)) + ',';
     } else if (i == 1) {
-      rgb += (Math.round(Math.random() * 255)) + ', ';
+      rgb += (Math.floor(Math.random() * 255)) + ',';
     } else if (i == 2) {
-      rgb += (Math.round(Math.random() * 255)) + ')';
+      rgb += (Math.floor(Math.random() * 255)) + ')';
     }
   }
   return rgb;
